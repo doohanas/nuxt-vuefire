6963330ed36af3280d416ab4209e42d48fc03cd4
Doohan <doohanaditya@yahoo.co.id>
2024-01-20 10:34:56 +0700

    chore: install firebase and nuxt-vuefire

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    // ... other modules
+    'nuxt-vuefire',
+  ],
+  vuefire: {
+    config: {
+      apiKey: process.env.FIREBARE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID
+      // there could be other properties depending on the project
+    },
+  },
 })
